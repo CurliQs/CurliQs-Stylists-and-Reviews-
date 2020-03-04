bb959640b9204de679f367984c89d654b6a90cec
Afia-Lydia <you@example.com>
2020-03-04 14:49:33 -0500

    created routes for login and registration for qs added qs and qutters to server via routes

--- api/routes/curliQ/Q-model.js
+++ api/routes/curliQ/Q-model.js
@@ -1,5 +1,36 @@
-const db = require('../../../database/dbConfig')
+const db = require("../../../database/dbConfig");
 
-module.exports ={
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById
+	
+};
+
+function find() {
+	return db("curliQ").select(
+		"curli_id",
+		"username",
+		"password",
+		"email",
+		"location",
+		"hair_type"
+	);
+}
+
+function findBy(filter){
+    return db("curliQ").where(filter)
+}
+
+function findById(curli_id) {
+	return db("curliQ")
+		.where({ curli_id })
+		.first();
+}
+
+async function add(user) {
+    const [curli_id] = await db("curliQ").insert(user);
     
+    return findById(curli_id)
 }
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const path = require('path')
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(express.static(path.join(__dirname, 'front-end/build')))
+server.use(express.static(path.join(__dirname, "front-end/build")));
 
+const curlyQRouter = require("./api/routes/curliQ/Q-router");
+const qutterRouter = require("./api/routes/curliQutter/Qutter-router");
 
-server.get('/', (req, res) => {
-  res.status(200).json({ status: "I'm alive and on fire :fireball:" })
-})
+server.use("/curliq", curlyQRouter);
+server.use("qutter", qutterRouter);
 
-server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/front-end/build/index.html'))
-  })
+server.get("/", (req, res) => {
+	res.status(200).json({ status: "I'm alive and on fire :fireball:" });
+});
+
+server.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/front-end/build/index.html"));
+});
 
 module.exports = server;
