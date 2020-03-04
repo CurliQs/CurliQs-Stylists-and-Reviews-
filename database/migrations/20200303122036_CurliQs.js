9b62ecc712356dca5bdb79530dc94349b573bfd1
Afia-Lydia <you@example.com>
2020-03-04 10:51:38 -0500

    completed first migration.

--- database/migrations/20200303122036_CurliQs.js
+++ database/migrations/20200303122036_CurliQs.js
@@ -7,55 +7,69 @@ exports.up = function(knex) {
 				.notNullable()
 				.unique();
 			t.string("password", 126).notNullable();
-			t.string("email", 256).notNullable();
+			t.string("email", 256)
+				.notNullable()
+				.unique();
 			t.string("location", 612);
 			t.string("hair_type", 126);
-		})
+    })
+    
 		.createTable("regimen", t => {
 			t.increments("regimen_id");
 
-			// t.integer("curli_id").unsigned();
+			t.integer("curli_id").unsigned();
 
 			t.foreign("curli_id")
-				.references("curli_id.curliQ")
+				.references("curli_id")
+				.inTable("curliQ")
 				.onDelete("CASCADE")
 				.onUpdate("CASCADE");
+
 			t.string("step", 1256);
-		})
-		.createTable("curliQutters", t => {
+    })
+    
+		.createTable("curliQutter", t => {
 			t.increments("qutter_id");
 			t.string("username", 255)
 				.notNullable()
 				.unique();
 			t.string("password", 126).notNullable();
-			t.string("email", 256).notNullable();
+			t.string("email", 256)
+				.notNullable()
+				.unique();
 			t.string("location", 612);
 			t.string("specialty", 1256);
-		})
+    })
+    
 		.createTable("portfolio", t => {
 			t.increments("portfolio_id");
-			// t.integer("qutter_id").unsigned();
+			t.integer("qutter_id").unsigned();
 			t.foreign("qutter_id")
-				.references("qutter_id.curliQutters")
+				.references("qutter_id")
+				.inTable("curliQutter")
 				.onDelete("CASCADE")
 				.onUpdate("CASCADE");
 			//json for image url
 			t.string("hair_type", 126);
 			t.string("products", 1256);
-		})
+    })
+    
 		.createTable("product_review", t => {
 			t.increments("review_id");
 			t.string("review", 1256);
 			t.string("rating", 126);
-			// t.interger("curli_id").unsigned();
+
+			t.integer("curli_id").unsigned()
 			t.foreign("curli_id")
-				.references("curli_id.curliQ")
+				.references("curli_id")
+				.inTable("curliQ")
 				.onDelete("CASCADE")
 				.onUpdate("CASCADE");
 
-			// t.interger("qutter_id").unsigned();
+			t.integer("qutter_id").unsigned()
 			t.foreign("qutter_id")
-				.references("qutter_id.curliQutters")
+				.references("qutter_id")
+				.inTable("curliQutter")
 				.onDelete("CASCADE")
 				.onUpdate("CASCADE");
 		});
