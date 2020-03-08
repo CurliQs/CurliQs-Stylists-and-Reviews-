415dd07701e75aa11ac6f973b42cbc1165b4a19c
Afia-Lydia <you@example.com>
2020-03-08 18:44:18 -0400

    User CRUD completed.

--- api/routes/curliQutters/Qutter-router.js
+++ api/routes/curliQutters/Qutter-router.js
@@ -1,37 +1,43 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const Qutter = require("./Qutter-model");
 
-const Qutter = require('./Qutter-model')
+router.post("/register", (req, res) => {
+	const body = req.body;
+	const hash = bcrypt.hashSync(body.password, 12);
+	body.password = hash;
+	Qutter.addQutter(body)
+		.then(nq => {
+			res.status(201).json(nq);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ message: "Error creating profile please add required data" });
+		});
+});
 
+router.get("/", (req, res) => {
+	Qutter.getQutters()
+		.then(qutter => {
+			res.status(200).json(qutter);
+		})
+		.catch(err => {
+			res.status(500).json({ message: `An Error has occured see ${err}` });
+		});
+});
 
-router.post('/', (req, res)=> {
-    const body = req.body
-    Qutter.addQutter(body)
-        .then(nq => {
-            res.status(201).json(nq)
-        })
-        .catch(err => {
-            res.status(500).json({message: 'Error creating profile please add required data'})
-        })
-    })
+router.get("/:id", (req, res) => {
+	const id = req.params.id;
+	Qutter.getQuttersId(id)
+		.then(qutter => {
+			res.status(200).json(id);
+		})
+		.catch(err => {
+			res.status(500).json({ message: `An Error has occured see ${err}` });
+		});
+});
 
-router.get('/', (req, res) => {
-    Qutter.getQutters()
-        .then(qutter => {
-            res.status(200).json(qutter)
-        })
-        .catch(err => {
-            res.status(500).json({message: `An Error has occured see ${err}`})
-        })
-})
-
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    Qutter.getQuttersId(id)
-        .then(qutter => {
-            res.status(200).json(id)
-        })
-        .catch(err => {
-            res.status(500).json({message: `An Error has occured see ${err}`})
-        })
-})
+module.exports = router;
