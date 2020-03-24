0d2b7ba214bf92c846a1b083814b0ac2e49e5cce
Ira <ira.d.sanchez@gmail.com>
2020-03-24 12:42:09 -0700

    Add NavLinks to Login/Register Buttons

--- front-end/src/components/External Pages/ExNav.js
+++ front-end/src/components/External Pages/ExNav.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton}  from '@material-ui/core';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function ButtonAppBar() {
@@ -22,14 +28,21 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-        
-          </IconButton>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
           <Typography variant="h6" className={classes.title}>
             Curli~Q's Styles'n'Reviews
           </Typography>
-          <Button color="inherit">Signin</Button>
-          <Button color="inherit">SignUp</Button>
+          <NavLink to="/register">
+            <Button color="inherit">Signin</Button>
+          </NavLink>
+          <NavLink to="/register">
+            <Button color="inherit">SignUp</Button>
+          </NavLink>
           <Button color="inherit">Curli~Q's</Button>
         </Toolbar>
       </AppBar>
--- front-end/src/components/External Pages/Marketing.js
+++ front-end/src/components/External Pages/Marketing.js
@@ -1,56 +1,82 @@
-import React from 'react'
-import { Avatar, Button, CssBaseline, Container, TextField, FormControlLabel, Checkbox, Link, CardMedia, Card, Grid, Typography, CardActions, CardContent } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import ExNav from './ExNav'
-import CN from '../../img/CurlNames.jpg'
-   
-const useStyles = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    
-  }));
+import React from "react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  Container,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  CardMedia,
+  Card,
+  Grid,
+  Typography,
+  CardActions,
+  CardContent
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ExNav from "./ExNav";
+import CN from "../../img/CurlNames.jpg";
 
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  }
+}));
 
 const Marketing = () => {
-
-    const classes = useStyles();
-    return (
-        <div>
-                <ExNav/>
-            <CssBaseline />
+  const classes = useStyles();
+  return (
+    <div>
+      <ExNav />
+      <CssBaseline />
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="md">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Craft your coils ... 
-            Suit your Style
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Craft your coils ... Suit your Style
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Here at Curli~Q Style'n'Reviews we care about every aspect of your coils. We know how hard it is to remember the perfect order of product to get the lucious bouncin' style. We know knowledgeable stylists are the key to maintinin' that do! We can help you with all of it!
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Here at Curli~Q Style'n'Reviews we care about every aspect of your
+              coils. We know how hard it is to remember the perfect order of
+              product to get the lucious bouncin' style. We know knowledgeable
+              stylists are the key to maintinin' that do! We can help you with
+              all of it!
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
@@ -65,37 +91,37 @@ const Marketing = () => {
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
-              <Grid item  xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Meet Our Stylists
-                    </Typography>
-                    <Typography>
-                    They are near you * They know your curl type * They have products for you to use
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item  xs={12} sm={6} md={4}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Meet Our Stylists
+                  </Typography>
+                  <Typography>
+                    They are near you * They know your curl type * They have
+                    products for you to use
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
               <Card>
                 <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Become a Culi~Q
-                    </Typography>
-                    <Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Become a Culi~Q
+                  </Typography>
+                  <Typography>
                     Care for your hair * Share the products that are working *
                     Save Products to try next time
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
         </Container>
       </main>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Marketing
+export default Marketing;
