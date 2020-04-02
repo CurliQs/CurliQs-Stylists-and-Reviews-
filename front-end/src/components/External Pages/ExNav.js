149a61a91306c150a052b233e36ac508b4b8e252
Ira <ira.d.sanchez@gmail.com>
2020-04-02 11:04:51 -0700

    Add Route for SignIn

--- front-end/src/components/External Pages/ExNav.js
+++ front-end/src/components/External Pages/ExNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
@@ -39,7 +40,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Curli~Q's Styles'n'Reviews
           </Typography>
-          <NavLink to="/register">
+          <NavLink to="/signin">
             <Button color="inherit">Signin</Button>
           </NavLink>
           <NavLink to="/register">
--- front-end/src/components/External Pages/Marketing.js
+++ front-end/src/components/External Pages/Marketing.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import { Avatar, Button, CssBaseline, Container, TextField, FormControlLabel, Checkbox, Link, CardMedia, Card, Grid, Typography, CardActions, CardContent } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import ExNav from './ExNav'
-import CN from '../../img/CurlNames.jpg'
-   
+import React from "react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  Container,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  CardMedia,
+  Card,
+  Grid,
+  Typography,
+  CardActions,
+  CardContent
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ExNav from "./ExNav";
+import CN from "../../img/CurlNames.jpg";
+
 const useStyles = makeStyles(theme => ({
-  
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
-    
-  }));
-
-const useStyles = makeStyles(theme => ({
   icon: {
     marginRight: theme.spacing(2)
   },
@@ -53,8 +63,17 @@ const Marketing = () => {
       <CssBaseline />
       <main>
         <div className={classes.heroContent}>
-<<<<<<< HEAD
-          <Container maxWidth="md">
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            align="center"
+            className={classes.title}
+          >
+            <img src={CN} />
+          </Typography>
+
+          <Container>
             <Typography
               component="h1"
               variant="h2"
@@ -62,17 +81,7 @@ const Marketing = () => {
               color="textPrimary"
               gutterBottom
             >
-              Craft your coils ... Suit your Style
-=======
-        <Typography component="h1" variant="h6" color="inherit" align='center'className={classes.title}>
-            <img src={CN}/>
-          </Typography>
-
-          <Container>
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Craft your Coils ... 
-            Suit your Style
->>>>>>> 796566bb50e838dcc40feabec1675657d9f20b4f
+              Craft your Coils ... Suit your Style
             </Typography>
             <Typography
               variant="h5"
@@ -88,80 +97,47 @@ const Marketing = () => {
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
-                <Grid item>
-                  
-                </Grid>
+                <Grid item></Grid>
               </Grid>
             </div>
           </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
-<<<<<<< HEAD
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm={6} md={4}>
+          <Grid container spacing={6} align="center">
+            <Grid item xs={12} sm={6} md={6}>
               <Card className={classes.card}>
                 <CardContent className={classes.cardContent}>
                   <Typography gutterBottom variant="h5" component="h2">
                     Meet Our Stylists
                   </Typography>
                   <Typography>
-                    They are near you * They know your curl type * They have
-                    products for you to use
+                    <li> They are near you</li>
+                    <li> They know your curl type</li>
+                    <li> They have products for you to use</li>
                   </Typography>
+                  <Button variant="contained" color="secondary">
+                    Become a Curli Sylist
+                  </Button>
                 </CardContent>
               </Card>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
+            <Grid item xs={12} sm={6} md={6}>
               <Card>
                 <CardContent className={classes.cardContent}>
                   <Typography gutterBottom variant="h5" component="h2">
                     Become a Culi~Q
                   </Typography>
                   <Typography>
-                    Care for your hair * Share the products that are working *
-                    Save Products to try next time
+                    <li>Care for your hair</li>
+                    <li>Share the products that are working</li>
+                    <li>Save products to try next time</li>
                   </Typography>
+                  <Button variant="contained" color="primary">
+                    Become a Curli~Q
+                  </Button>
                 </CardContent>
               </Card>
             </Grid>
-=======
-          <Grid container spacing={6} align="center">
-              <Grid item  xs={12} sm={6} md={6}>
-                <Card className={classes.card}>
-
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Meet Our Stylists
-                    </Typography>
-                    <Typography>
-                      <li> They are near you</li>
-                      <li> They know your curl type</li>
-                      <li> They have products for you to use</li> 
-                    </Typography>
-                    <Button variant="contained" color="secondary">
-                    Become a Curli Sylist
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item  xs={12} sm={6} md={6}>
-              <Card>
-                <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Become a Culi~Q
-                    </Typography>
-                    <Typography>
-                      <li>Care for your hair</li>
-                      <li>Share the products that are working</li>
-                      <li>Save products to try next time</li>
-                    </Typography>
-                    <Button variant="contained" color="primary">
-                      Become a Curli~Q
-                      </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
->>>>>>> 796566bb50e838dcc40feabec1675657d9f20b4f
           </Grid>
         </Container>
       </main>
